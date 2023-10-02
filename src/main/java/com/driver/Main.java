2f65fd2125d943041e5afff15a8497f4e1da4616
HarryPotter0107 <bhanuupadhyay87@gamil.com>
2023-10-02 15:06:20 +0530

    message change

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,19 +1,5 @@
 package com.driver;
 
-public class Main {
-    public static void main(String[] args) {
-        B objB = new B();
-
-        // Calling the method from class A
-        String resultFromA = objB.meth();
-        System.out.println("Result from class A: " + resultFromA);
-
-        // Calling the overridden method from class B
-        String resultFromB = objB.meth();
-        System.out.println("Result from class B: " + resultFromB);
-    }
-}
-
 class A {
     String meth() {
         return "Invoking method from class A";
@@ -24,5 +10,19 @@ class B extends A {
     @Override
     String meth() {
         return "Method is overridden in Extended class B";
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        B objB = new B();
+
+        // Calling the method from class A using objB
+        String resultFromA = objB.meth();
+        System.out.println(resultFromA);
+
+        // Calling the overridden method from class B using objB
+        String resultFromB = objB.meth();
+        System.out.println( resultFromB);
     }
 }
